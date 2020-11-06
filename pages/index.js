59aea8bd97584ce16ff4ccb14afa3973961693a6
Nayeli Perez <nayeli.perez@chargify.com>
2020-11-06 14:46:48 -0600

    delete unecessary paragraph in index

--- pages/index.js
+++ pages/index.js
@@ -23,12 +23,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-
         <p>I’m a multidisciplinary designer focused on product design. I enjoy photography, art, fashion, and thinking about history, sustainability, and the design process.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
